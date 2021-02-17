2fa40ff2f6513eb2ed328d5e414807264e265bca
AmaniMokni <AmaniMokni@github.com>
2021-02-17 23:55:28 +0100

    Exercice 2

--- Desktop/bataille-navale/src/test/java/ensta/TestBoard.java
+++ Desktop/bataille-navale/src/test/java/ensta/TestBoard.java
@@ -4,10 +4,10 @@ import ensta.Board;
 
 public class TestBoard {
     public static void main(String[] args){
-        String name="ensta.Board 1";
+        String name="Board 1";
         int taille= 7;
         Board B1 = new Board(name,taille);
-        Board B2 = new Board("ensta.Board 2");
+        Board B2 = new Board("Board 2");
         B1.print();
         B2.print();
     }
